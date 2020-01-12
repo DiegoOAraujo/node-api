ad766e1fab3fc47c7e00eb33c5d3b2a49887e6cc
Diego Araujo <doasalah@gmail.com>
2020-01-12 13:55:07 -0300

    criando model produto e ajustando rotas

--- server.js
+++ server.js
@@ -6,26 +6,18 @@ const requireDir = require('require-dir');
 const app = express();
 
 //iniciando o DB
-mongoose.connect(
-  'mongodb://localhost:27017/nodeapi',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+try {
+  mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+} catch (error) {
+  console.warn('Erro ao conectar no banco');
+}
 
 requireDir("./src/models");
 
-const Product = mongoose.model('Product');
-
-
-
-//primeira rota '/' raiz
-app.get('/', (req, res) => {
-Product.create({
-  title: 'React Native',
-  description:'Build native apps with React',
-  url: 'http://github.com/facebook/react-native'
-});
-
-  return res.send('Oi Diego');
-});
+//Rotas
+app.use('/api',require("./src/routes"));
 
 app.listen(3001);
