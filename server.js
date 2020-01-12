ededc324e7bda8dcb5c17ed1e50642481f423ca5
DiegoOAraujo <doasalah@gmail.com>
2020-01-12 00:25:56 -0300

    primeira conexao com banco de dados

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o app
 const app = express();
 
 //iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+
+
 
 //primeira rota '/' raiz
-app.get('/', (req, res)=>{
-  res.send('Oi Diego');
+app.get('/', (req, res) => {
+Product.create({
+  title: 'React Native',
+  description:'Build native apps with React',
+  url: 'http://github.com/facebook/react-native'
+});
+
+  return res.send('Oi Diego');
 });
 
 app.listen(3001);
